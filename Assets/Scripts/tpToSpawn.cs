5511a2b8bfe39d2233b3ed2b7b93245a8fa813da
Derrick Quiamas <quiamasderrick@gmail.com>
2025-10-05 00:43:00 -1000

    finished the tp to spawn script
    added a bounds and if it passes , tp to spawn

--- Assets/Scripts/tpToSpawn.cs
+++ Assets/Scripts/tpToSpawn.cs
@@ -6,6 +6,11 @@ public class PlayerBody : MonoBehaviour
 
     public Vector3 spawnPoint;
     float minY = -5f;
+    float maxZ = 1414f;
+    float minZ = 991f;
+
+    float maxX = 1577f;
+    float minX = 1170f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,7 +21,10 @@ public class PlayerBody : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (cam.transform.position.y < minY)
+        if (cam.transform.position.y < minY ||
+            cam.transform.position.z > maxZ || cam.transform.position.z < minZ ||
+            cam.transform.position.x > maxX || cam.transform.position.x < minX
+        )
         {
             transform.position = spawnPoint + Vector3.up * .2f;
         }
