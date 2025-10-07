7214e315d073b49ab0584e021fe3b98f424f5fe2
Derrick Quiamas <quiamasderrick@gmail.com>
2025-10-06 18:49:50 -1000

    finished next core mechanic
    finished inventory/slot system

--- Assets/Scripts/GrabHerbBehavior.cs
+++ Assets/Scripts/GrabHerbBehavior.cs
@@ -14,11 +14,21 @@ public class GrabHerbBehavior : MonoBehaviour
     public GameObject inv;
     public InventoryScript invScript;
 
+    public bool isInInv = false;
+    public bool beingTouched = false;
+
+    public GameObject toFollow;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        invScript = inv.GetComponent<InventoryScript>();
+        inv = GameObject.FindWithTag("inventory");
+        if (inv != null)
+        {
+            invScript = inv.GetComponent<InventoryScript>();
+        }
+
     }
 
     // Update is called once per frame
@@ -35,31 +45,70 @@ public class GrabHerbBehavior : MonoBehaviour
         }
 
         touched = grabScript.GrabPoints.Count > 0;
+
+
+
+        // behavior for while it is in inventory
+        if (isInInv && !beingTouched)
+        {
+            followCam();
+        }
     }
 
     public void OnGrab()
     {
         Renderer render = herb.GetComponent<Renderer>();
 
-        render.material.color = Color.red;
+        if (!isInInv)
+        {
+            render.material.color = Color.red;
+        }
+        else
+        {
+            beingTouched = true;
+            render.material.color = Color.purple;
+        }
+
     }
 
     public void OnRelease()
     {
         Renderer render = herb.GetComponent<Renderer>();
-
-        render.material.color = Color.green;
-
-        if (invScript.currentitemCount < 2)
+        if (!isInInv)
         {
-            render.material.color = Color.pink; // to visually debug the backend
-            // this means while testing, at most you should only see 2 pink cubes since i havent made the delete from inv yet
+            render.material.color = Color.green;
 
-            invScript.inv[invScript.currentitemCount] = Instantiate(herb);
-            invScript.currentitemCount++;
+            if (inv != null && invScript.currentitemCount < 2)
+            {
+                render.material.color = Color.pink; // to visually debug the backend
+                // this means while testing, at most you should only see 2 pink cubes since i havent made the delete from inv yet
+                isInInv = true;
+                invScript.currentitemCount++;
+
+                // invariant: if currentitemcount < 2,  slot must be available
+                if (!invScript.slot1Taken)
+                {
+                    invScript.slot1Taken = true;
+                    toFollow = invScript.slot1;
+                }
+                else if (!invScript.slot2Taken)
+                {
+                    invScript.slot2Taken = true;
+                    toFollow = invScript.slot2;
+                }
+
+                spawnerScript.currentplants--;
+            }
+            else
+            {
+                StartCoroutine(resetpos(1f));
+            }
         }
-
-        StartCoroutine(resetpos(1f));
+        else
+        {
+            beingTouched = false; // released so allow followCam()
+            render.material.color = Color.pink;
+        }
     }
 
     private IEnumerator resetpos(float delay)
@@ -67,6 +116,12 @@ public class GrabHerbBehavior : MonoBehaviour
         yield return new WaitForSeconds(delay);
         Destroy(herb);
         spawnerScript.currentplants--;
+    }
+
+    public void followCam()
+    {
+        transform.position = toFollow.transform.position;
+        transform.rotation = toFollow.transform.rotation;
     }
 
 }
